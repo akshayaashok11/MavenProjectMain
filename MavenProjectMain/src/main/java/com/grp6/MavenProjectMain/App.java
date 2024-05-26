f4e96b207b09c20bc8642ade8a8c5948aaef7b2a
Akshaya <akshayaashok1109@gmail.com>
2024-05-26 11:29:29 +0530

    Add files

---
+++ MavenProjectMain/src/main/java/com/grp6/MavenProjectMain/App.java
@@ -0,0 +1,13 @@
+package com.grp6.MavenProjectMain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
